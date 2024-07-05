aea24ac2b1701934342e345d479d4e8ced258882
soreendev <soreendev@gmail.com>
2024-07-05 13:20:45 +0430

    modify json output

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        return EventResource::collection( Event::with('user','attendees')->get() );
     }
 
     /**
@@ -33,7 +34,8 @@ class EventController extends Controller
             'user_id' =>1
 
         ]);
-        return $event;
+        $event->load('user','attendees');
+        return new EventResource( $event );
     }
 
     /**
@@ -41,7 +43,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        $event->load('user','attendees');
+        return new EventResource($event);
     }
 
     /**
@@ -59,7 +62,8 @@ class EventController extends Controller
                 ]
             ));
 
-        return $event ;
+        $event->load('user','attendees');
+        return new EventResource($event);
     }
 
     /**
